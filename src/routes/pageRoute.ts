767d1c658f5025d709d7c645fdce3dda8aa2055a
CDavidSV <88672259+CDavidSV@users.noreply.github.com>
2023-05-26 10:40:39 -0600

    Update pageRoute.ts

--- src/routes/pageRoute.ts
+++ src/routes/pageRoute.ts
@@ -2,7 +2,7 @@ import router from './router';
 import path = require('path');
 
 router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './page/index.html'));
+  res.sendFile(path.join(__dirname, '../public/login.html'));
 });
 
 export default router;
